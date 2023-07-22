de70df9bbdeaee33aeb6607d443d2c1e74598ada
iagasi <nameagasi@gmail.com>
2023-07-22 19:40:09 +0400

    test

--- server/src/resolvers/authController.ts
+++ server/src/resolvers/authController.ts
@@ -39,7 +39,7 @@ console.log("log-here");
         httpOnly: false,
         maxAge: 1 ,
        sameSite:"none",
-       domain:"instagram-urgy.onrender.com",
+       domain:".onrender.com",
        secure:true,
       };
       res.cookie(cookieName, resUser?.refreshToken, cookieOptions);
